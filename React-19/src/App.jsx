10ad1f312b361e15d6f57fa1508da9835fe10e47
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2024-12-28 10:52:25 +0500

    learningReact19

---
+++ React-19/src/App.jsx
@@ -0,0 +1,27 @@
+import { Suspense, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/Login'
+import Theme from './useHook/context'
+import Opotimistic from './useHook/opotimistic'
+import Joke from './useHook/use'
+import JokeItem from './useHook/use'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <Joke /> */}
+      {/* <Theme /> */}
+      {/* <Opotimistic /> */}
+      {/* <Suspense fallback={"loading"}>
+        <JokeItem />
+      </Suspense> */}
+    </>
+  )
+}
+
+export default App
